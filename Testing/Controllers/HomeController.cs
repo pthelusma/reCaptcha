b7776acf65e9ffaefb8e437eba8db0357a0c4ff6
pthelusma <pthelusma@gmail.com>
2014-06-20 21:02:16 -0400

    Initial reCaptcha code

---
+++ Testing/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Submit(string remoteip, string privatekey, string challenge, string response)
+        {
+
+             var something = verifyRecaptcha(remoteip, privatekey, challenge, response);
+
+            return Json(new {Result = "Success"});
+        }
+
+        private async Task<string> verifyRecaptcha(string remoteip, string privatekey, string challenge, string response)
+        {
+
+            HttpClient client = new HttpClient();
+
+            var values = new List<KeyValuePair<string, string>>();
+            values.Add(new KeyValuePair<string, string>("remoteip", HttpUtility.UrlEncode(remoteip)));
+            values.Add(new KeyValuePair<string, string>("privatekey", HttpUtility.UrlEncode(privatekey)));
+            values.Add(new KeyValuePair<string, string>("challenge", HttpUtility.UrlEncode(challenge)));
+            values.Add(new KeyValuePair<string, string>("response", HttpUtility.UrlEncode(response)));
+
+            var content = new FormUrlEncodedContent(values);
+            var resp = await client.PostAsync("http://www.google.com/recaptcha/api/verify", content).ConfigureAwait(false);
+            var responseString = await resp.Content.
+
+            return responseString;
+  
+        }
+
+    }
+}
---
+++ Testing/Models/MyModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Testing.Models
+{
+    [Serializable]
+    public class MyModel
+    {
+        [Required(ErrorMessage = "*Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        public string Phone { get; set; }
+    }
+}
